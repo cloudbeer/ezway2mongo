f1cd226fd7ecaf4a84e4f5479977c57b173d9371
谢正伟 <xiezhengwei@youku.com>
2016-03-24 10:39:18 +0800

    重大更新，假如了一个 koa 插件，可以不用写任何代码直接假如了 增删改查的 rest api

--- test.js
+++ test.js
@@ -1,11 +1,35 @@
 var ezmongo = require('./lib');
 
-ezmongo.connect('mongodb://localhost:27017/wdm');
+ezmongo.connect('mongodb://10.163.11.20:27017/omd');
 
 
 setTimeout(function () {
-  var co = ezmongo.find('store');
+  // var co = ezmongo.find('order',
+  //   {buyer_name: /.*啤酒.*/},
+  //   {
+  //     skip   : 4995,
+  //     limit  : 1,
+  //     sort   : {_id: -1},
+  //     project: {_id: 1}
+  //   });
+  // co(function (err, rres) {
+  //   console.log(rres);
+  // });
+
+  var co = ezmongo.deleteOne('test');
   co(function (err, rres) {
-    console.log(err, rres);
+    console.log(rres);
   });
+  //console.log(co.next());
+
+  // var co = ezmongo.insert('test', [
+  //   {    a: 1, b: 21  },
+  //   {    a: 2, b: 22  },
+  //   {    a: 3, b: 23  }
+  // ]);
+  // co(function (err, rres) {
+  //   console.log(rres);
+  // });
+
+
 }, 1000);
